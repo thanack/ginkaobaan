7f894845a51c2cb163d0e145cc6b77e91318eefd
thanack <thanaphat2543@gmail.com>
2020-12-25 17:22:09 +0700

    Lab6 No.3

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,18 +1,43 @@
-//Dialog of the first example is given below.
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+#include <iostream>
+#include<string>
+using namespace std;
+
+
+int main() {
+ 
+  string name;
+  cout << "Fahsai: Sawadee ka...Can you tell me your name?" << "\n";
+  cout << "?????: ";
+  getline(cin,name);
+  cout << "Fahsai: Wow!!! "<< name << " is a really cool name." << "\n";
+  
+  cout << "Fahsai: I think you are an Engineering student. What is your student ID?" << "\n";
+  cout << name << ": " ;
+  int id;
+  cin >> id;
+
+  int gear;
+      gear = (id/10000000)-12;
+  cout << "Fahsai: I think you may be GEAR " << gear << ". I have a free movie ticket for you." << "\n";
+
+  string movie;
+  cout << "Fahsai: Let's go to the cinema together!!!" << "\n";
+  cout << "Fahsai: What movie do you want to watch?" << "\n";
+  cout << name << ": ";
+  cin.ignore();
+  getline(cin,movie);
+
+  string day;
+    cout << "Fahsai: So....which day are you free to go with me?" << "\n";
+    cout << name << ": ";
+    getline(cin,day);
+    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you." << "\n";
+
+  string text;
+    cout << name << ": ";
+    getline(cin,text);
+    cout << "Fahsai: 555+ see you "<< day << ". Bye Bye \\(^ ^)/";
+
+    return 0;
+}

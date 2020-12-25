5b368d9cae85aaf0c11ea12d5950ac45d384f500
thanack <thanaphat2543@gmail.com>
2020-12-25 15:40:45 +0700

    Lab No.2

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+    double a  =  20; 
+    double b = 30;
+    double c = 4;
+    double output;
+    output = (a + b) / c;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+    cout << "output: " << output << "\n";
     return 0;
 }
